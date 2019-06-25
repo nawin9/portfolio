752a1da3f0deecd2613bbe6b753ccc18cc069ea4
nawin.kim@epitech.eu <nawin.kim@epitech.eu>
2019-06-25 21:35:40 +0200

    try fixing page routing

--- next.config.js
+++ next.config.js
@@ -4,33 +4,13 @@ const withImages = require('next-images');
 
 const debug = process.env.NODE_ENV !== 'production';
 
-// module.exports = compose([
-//     [withCss, {}],
-//     [withPurgeCss, {}],
-//     [withImages, {}],
-//     {
-//         exportPathMap() {
-//             return {
-//                 '/': { page: '/' },
-//                 '/portfolio': { page: '/portfolio' },
-//             };
-//         },
-//         assetPrefix: !debug ? 'https://nawin9.github.io/portfolio/' : '',
-//     },
-//     {
-//         env: {
-//             BACKEND_URL: debug ? '' : '/portfolio',
-//         },
-//     },
-// ]);
-
 module.exports = withCss(
     withPurgeCss(
         withImages({
             exportPathMap: () => ({
                 '/': { page: '/' },
             }),
-            assetPrefix: !debug ? 'https://nawin9.github.io/portfolio/' : '',
+            assetPrefix: !debug ? '/portfolio' : '',
             env: {
                 BACKEND_URL: debug ? '' : '/portfolio',
             },
