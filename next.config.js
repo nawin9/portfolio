75076249a600c6f545f9275dceda7532b6882c42
nawin.kim@epitech.eu <nawin.kim@epitech.eu>
2019-06-25 02:41:30 +0200

    ignore next compose

--- next.config.js
+++ next.config.js
@@ -5,24 +5,37 @@ const compose = require('next-compose');
 
 const debug = process.env.NODE_ENV !== 'production';
 
-module.exports = compose([
-    [withCss, {}],
-    [withPurgeCss, {}],
-    [withImages, {}],
-    {
-        exportPathMap() {
-            return {
+// module.exports = compose([
+//     [withCss, {}],
+//     [withPurgeCss, {}],
+//     [withImages, {}],
+//     {
+//         exportPathMap() {
+//             return {
+//                 '/': { page: '/' },
+//                 '/portfolio': { page: '/portfolio' },
+//             };
+//         },
+//         assetPrefix: !debug ? 'https://nawin9.github.io/portfolio/' : '',
+//     },
+//     {
+//         env: {
+//             BACKEND_URL: debug ? '' : '/portfolio',
+//         },
+//     },
+// ]);
+
+module.exports = withCss(
+    withPurgeCss(
+        withImages({
+            exportPathMap: () => ({
                 '/': { page: '/' },
                 '/portfolio': { page: '/portfolio' },
-            };
-        },
-        assetPrefix: !debug ? 'https://nawin9.github.io/portfolio/' : '',
-    },
-    {
-        env: {
-            BACKEND_URL: debug ? '' : '/portfolio',
-        },
-    },
-]);
-
-// module.exports = withCss(withPurgeCss(withImages()))
+            }),
+            assetPrefix: !debug ? 'https://nawin9.github.io/portfolio/' : '',
+            env: {
+                BACKEND_URL: debug ? '' : '/portfolio',
+            },
+        }),
+    ),
+);
