ff41ccdd0de715cabb7a79a842dc69aaeeec17b3
nawin.kim@epitech.eu <nawin.kim@epitech.eu>
2019-06-25 02:36:13 +0200

    reconfig next

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
 const withCss = require('@zeit/next-css');
 const withPurgeCss = require('next-purgecss');
 const withImages = require('next-images');
+const compose = require('next-compose');
 
-module.exports = withCss(withPurgeCss(withImages()));
+const debug = process.env.NODE_ENV !== 'production';
+
+module.exports = compose([
+    [withCss, {}],
+    [withPurgeCss, {}],
+    [withImages, {}],
+    {
+        exportPathMap() {
+            return {
+                '/': { page: '/' },
+                '/portfolio': { page: '/portfolio' },
+            };
+        },
+        assetPrefix: !debug ? 'https://nawin9.github.io/portfolio/' : '',
+    },
+    {
+        env: {
+            BACKEND_URL: debug ? '' : '/portfolio',
+        },
+    },
+]);
+
+// module.exports = withCss(withPurgeCss(withImages()))
