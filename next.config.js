2f534765233516210bae0910b55e438cc7fa3c0b
nawin.kim@epitech.eu <nawin.kim@epitech.eu>
2019-06-25 02:47:08 +0200

    remove useless plugin

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const withCss = require('@zeit/next-css');
 const withPurgeCss = require('next-purgecss');
 const withImages = require('next-images');
-const compose = require('next-compose');
 
 const debug = process.env.NODE_ENV !== 'production';
 
